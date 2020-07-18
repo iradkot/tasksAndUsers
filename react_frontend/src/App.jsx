bb6d225c8de3c60d7fb8092a0e2cddcc43c61512
iradkot <irad16@gmail.com>
2020-07-18 16:03:24 +0300

    feat(react_frontend): add styled-components and axios

---
+++ react_frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+const TitleText = styled.h1``;
+
+function App() {
+  return (
+    <div className="App">
+        <TitleText>Tasks manager</TitleText>
+    </div>
+  );
+}
+
+export default App;
