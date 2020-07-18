a3d7c7e56e1347f1008caeb427cc240f6c381dcf
iradkot <irad16@gmail.com>
2020-07-18 16:23:02 +0300

    feat(mui input): add mui input comopnent

--- react_frontend/src/App.jsx
+++ react_frontend/src/App.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
+import Input from './components/Input';
 
-const TitleText = styled.h1``;
+const Container = styled.div`
+  width: 100%;;
+  height: 100vh;
+  display: grid;
+  justify-content: center;
+  
+`;
+
+const TitleText = styled.h1`
+  width: max-content;
+`;
 
 function App() {
-  return (
-    <div className="App">
-        <TitleText>Tasks manager</TitleText>
-    </div>
-  );
+    const [ description, setDescription ] = useState('');
+    const [ error, setError ] = useState('');
+    const handleChangeDesc = useCallback((e) => setDescription(e.target.value), [])
+    return (
+        <Container>
+            <TitleText>Tasks manager</TitleText>
+            <Input value={ description } onChange={ handleChangeDesc } label="Add task" error={error}/>
+        </Container>
+    );
 }
 
 export default App;
---
+++ react_frontend/src/components/Input.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+const Input = ({ error, label, value, onChange }) => (
+    <TextField id={ label } value={ value } onChange={ onChange }
+               label={ label } error={ error } helperText={ error }/>
+);
+
+export default Input;
