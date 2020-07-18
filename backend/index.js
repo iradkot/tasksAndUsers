1bfbbc0dc1bc9d8747d7a0452f6c399eb6008f06
iradkot <irad16@gmail.com>
2020-07-18 12:16:54 +0300

    feat(db-add task): add add task route and handling

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,22 @@ app.use(express.json());
 
 // ROUTES //
  //create a todo
+
+app.post('/tasks', async(req, res) => {
+    try {
+        const { description } = req.body;
+        console.log({description});
+        const newTask = await pool.query(
+            "INSERT INTO tasks (description) VALUES($1) RETURNING *",
+            [description]
+        );
+        
+        res.json(newTask.rows[0]);
+    } catch (e) {
+        console.log('todos post e:', e);
+    }
+})
+
  //get all todos
  //get a todo
  //update a todo
