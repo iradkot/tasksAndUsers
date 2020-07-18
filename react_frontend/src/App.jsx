50d58602b75b6e6d89ba65aa9a4ba4349fdbfcf2
iradkot <irad16@gmail.com>
2020-07-18 17:51:42 +0300

    feat(frontend: display list): display list of items + basic design

--- react_frontend/src/App.jsx
+++ react_frontend/src/App.jsx
@@ -1,27 +1,78 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddCircleOutline from '@material-ui/icons/AddCircleOutline';
+
+import * as apiRequests from './api/requests';
 import Input from './components/Input';
 
 const Container = styled.div`
-  width: 100%;;
+  width: 100%;
+  min-width: max-content;
   height: 100vh;
-  display: grid;
-  justify-content: center;
-  
+      display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 const TitleText = styled.h1`
   width: max-content;
 `;
+const AddTaskForm = styled.div`
+  display: flex;
+`;
 
 function App() {
+    const [ tasksData, setTaskData ] = useState([]);
     const [ description, setDescription ] = useState('');
     const [ error, setError ] = useState('');
-    const handleChangeDesc = useCallback((e) => setDescription(e.target.value), [])
+    const handleChangeDesc = useCallback((e) => setDescription(e.target.value), []);
+    
+    const getAllTasks = useCallback(async () => {
+        try {
+            const { data } = await apiRequests.getAllTasks();
+            setTaskData(data);
+        } catch (e) {
+            console.log({ e })
+        }
+    }, []);
+    
+    useEffect(() => {
+        getAllTasks();
+    }, []);
+    
     return (
         <Container>
             <TitleText>Tasks manager</TitleText>
-            <Input value={ description } onChange={ handleChangeDesc } label="Add task" error={error}/>
+            <AddTaskForm>
+                <Input value={ description } onChange={ handleChangeDesc } label="Add task" error={ error }/>
+                <IconButton edge="start" aria-label="edit">
+                    <AddCircleOutline/>
+                </IconButton>
+            </AddTaskForm>
+            <List>
+                { tasksData.map(({ description, task_id }) => (
+                    <ListItem key={ task_id }>
+                        <IconButton edge="start" aria-label="edit">
+                            <EditIcon/>
+                        </IconButton>
+                        <ListItemText
+                            primary={ description + '-' + task_id }
+                        />
+                        <ListItemSecondaryAction>
+                            <IconButton edge="end" aria-label="delete">
+                                <DeleteIcon/>
+                            </IconButton>
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                )) }
+            </List>
         </Container>
     );
 }
--- react_frontend/src/components/Input.jsx
+++ react_frontend/src/components/Input.jsx
@@ -3,7 +3,7 @@ import TextField from '@material-ui/core/TextField';
 
 const Input = ({ error, label, value, onChange }) => (
     <TextField id={ label } value={ value } onChange={ onChange }
-               label={ label } error={ error } helperText={ error }/>
+               label={ label } error={ !!error } helperText={ error }/>
 );
 
 export default Input;
